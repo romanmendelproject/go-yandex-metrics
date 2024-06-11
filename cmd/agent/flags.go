e7c0f3c67784f91b3d52ec35ef6b52a13e52f8c6
romanmendel <romanmendel777@gmail.com>
2024-06-11 21:25:51 +0300

    fix url flags

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -9,7 +9,7 @@ var reportInterval int
 var pollInterval int
 
 func parseFlags() {
-	flag.StringVar(&flagReqAddr, "a", ":8080", "address and port to run agent")
+	flag.StringVar(&flagReqAddr, "a", "localhost:8080", "address and port to run agent")
 	flag.IntVar(&reportInterval, "r", 10, "send metrics to server")
 	flag.IntVar(&pollInterval, "p", 2, "collect metrics from runtime")
 	flag.Parse()
