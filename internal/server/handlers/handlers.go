a9787c035df3b9a2ff2a218c35b9dbc1a733d509
romanmendel <romanmendel777@gmail.com>
2024-07-01 21:48:55 +0300

    add WriteHeader

--- internal/server/handlers/handlers.go
+++ internal/server/handlers/handlers.go
@@ -207,6 +207,7 @@ func (h *ServiceHandlers) AllData(res http.ResponseWriter, req *http.Request) {
 	values := h.storage.GetAll()
 
 	res.Header().Set("Content-Type", "text/html")
+	res.WriteHeader(http.StatusOK)
 	for i, value := range values {
 		io.WriteString(res, fmt.Sprintf("%d type = %s  name = %s value = %v", i, value.Type, value.Name, value.Value))
 	}
